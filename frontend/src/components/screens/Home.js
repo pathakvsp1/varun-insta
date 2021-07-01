f059a8fa9039ee0bf969d8f0df2a71c01564465a
Pathakvsp1 <Pathakvsp1@gmail.com>
2021-07-01 19:12:24 +0530

    uploading

---
+++ frontend/src/components/screens/Home.js
@@ -0,0 +1,206 @@
+import React,{useState,useEffect,useContext} from 'react'
+import {UserContext} from '../../App'
+import {Link} from 'react-router-dom'
+const Home  = ()=>{
+    const [data,setData] = useState([])
+    const {state,dispatch} = useContext(UserContext)
+    useEffect(()=>{
+       fetch('/allpost',{
+           headers:{
+               "Authorization":"Bearer "+localStorage.getItem("jwt")
+           }
+       }).then(res=>res.json())
+       .then(result=>{
+           console.log(result)
+           setData(result.posts)
+       })
+    },[])
+
+    const likePost = (id)=>{
+          fetch('/like',{
+              method:"put",
+              headers:{
+                  "Content-Type":"application/json",
+                  "Authorization":"Bearer "+localStorage.getItem("jwt")
+              },
+              body:JSON.stringify({
+                  postId:id
+              })
+          }).then(res=>res.json())
+          .then(result=>{
+                   //   console.log(result)
+            const newData = data.map(item=>{
+                if(item._id===result._id){
+                    return result
+                }else{
+                    return item
+                }
+            })
+            setData(newData)
+          }).catch(err=>{
+              console.log(err)
+          })
+    }
+    const unlikePost = (id)=>{
+          fetch('/unlike',{
+              method:"put",
+              headers:{
+                  "Content-Type":"application/json",
+                  "Authorization":"Bearer "+localStorage.getItem("jwt")
+              },
+              body:JSON.stringify({
+                  postId:id
+              })
+          }).then(res=>res.json())
+          .then(result=>{
+            //   console.log(result)
+            const newData = data.map(item=>{
+                if(item._id===result._id){
+                    return result
+                }else{
+                    return item
+                }
+            })
+            setData(newData)
+          }).catch(err=>{
+            console.log(err)
+        })
+    }
+
+    const makeComment = (text,postId)=>{
+          fetch('/comment',{
+              method:"put",
+              headers:{
+                  "Content-Type":"application/json",
+                  "Authorization":"Bearer "+localStorage.getItem("jwt")
+              },
+              body:JSON.stringify({
+                  postId,
+                  text
+              })
+          }).then(res=>res.json())
+          .then(result=>{
+              console.log(result)
+              const newData = data.map(item=>{
+                if(item._id===result._id){
+                    return result
+                }else{
+                    return item
+                }
+             })
+            setData(newData)
+          }).catch(err=>{
+              console.log(err)
+          })
+    }
+
+    const deletePost = (postid)=>{
+        fetch(`/deletepost/${postid}`,{
+            method:"delete",
+            headers:{
+                Authorization:"Bearer "+localStorage.getItem("jwt")
+            }
+        }).then(res=>res.json())
+        .then(result=>{
+            console.log(result)
+            const newData = data.filter(item=>{
+                return item._id !== result._id
+            })
+            setData(newData)
+        })
+    }
+   return (
+    <>
+    {data ?
+    <>
+    <div className="home">
+           {
+               data.map(item=>{
+                   return(
+                       <div className="card home-card" key={item._id}>
+                            <h5 className="post-username" style={{padding:"15px 15px"}}><Link to={item.postedBy._id !== state._id?"/profile/"+item.postedBy._id :"/profile"  }>{item.postedBy.name}</Link> {item.postedBy._id === state._id 
+                            && <i className="material-icons" style={{
+                                float:"right",
+                                paddingRight: "15px"
+                            }} 
+                            onClick={()=>deletePost(item._id)}
+                            >delete</i>
+
+                            }</h5>
+                            <div className="card-image">
+                                <img src={item.photo} alt="card-pic"/>
+                            </div>
+                            <div className="card-content">
+                            {item.likes.includes(state._id)
+                            ? 
+                             <i className="material-icons" style={{color:"red"}}
+                                    onClick={()=>{unlikePost(item._id)}}
+                              >favorite</i>
+                            : 
+                            <i className="material-icons"
+                            onClick={()=>{likePost(item._id)}}
+                            >favorite_border</i>
+                            }&nbsp;&nbsp;
+                            <i className="material-icons"
+                            onClick={()=>{
+                                {var cid = "comment";}
+                                {cid+=item._id}                                
+                                document.getElementById(cid).style.display = "block";
+                            }}
+                            >comment</i>                           
+                           
+                                <h6>{item.likes.length} Likes , {item.comments.length} Comments</h6>
+                                <p><span style={{fontWeight:"500"}}>{item.postedBy.name}</span> {item.title}</p>
+                                <p>{item.body}</p>
+                                <div id={"comment" + item._id} style={{display:"none"}}>
+                                {
+                                    item.comments.map(record=>{
+                                        return(
+                                        <h6 key={record._id}><span style={{fontWeight:"500"}}>{record.postedBy.name}</span> {record.text}</h6>
+                                        )
+                                    })
+                                }
+                                </div>
+                                <hr/>
+                                <form className="comment" onSubmit={(e)=>{
+                                    e.preventDefault()
+                                    makeComment(e.target[0].value,item._id)
+                                    e.target[0].value=""
+                                }}>
+                                  <input type="text" id="comment" placeholder="Add a Comment" />  
+                                </form>
+                                
+                            </div>
+                        </div> 
+                   )
+               })
+           }
+          
+          
+       </div>
+       {/* <div className="home">
+       {
+           <div className="card home-card">
+              <h4> People You may Know :</h4>
+               {
+               data.map(item=>{
+                   return(
+                       <>
+                    <h5 className="post-username" style={{padding:"5px"}}><Link to={item.postedBy._id !== state._id?"/profile/"+item.postedBy._id :"/profile"  }>{item.postedBy.name}</Link> {item.postedBy._id === state._id}</h5>
+                    <br/>
+                        </>
+                )
+               })
+            }
+           </div>
+           }
+       </div> */}
+</>
+   : <h2>loading...!</h2>}
+       
+   </>
+   )
+}
+
+
+export default Home
---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const {ObjectId} = mongoose.Schema.Types
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    resetToken:String,
+    expireToken:Date,
+    pic:{
+     type:String,
+     default:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tenforums.com%2Ftutorials%2F90186-change-default-account-picture-windows-10-a.html&psig=AOvVaw2e203UC-7r32IHDGEHwEY2&ust=1623698175153000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKjOiJuplfECFQAAAAAdAAAAABAD"
+    },
+    bio:{
+        type:String,
+        default:"Hi there , I'm using My Insta."
+       },
+    followers:[{type:ObjectId,ref:"User"}],
+    following:[{type:ObjectId,ref:"User"}]
+})
+
+mongoose.model("User",userSchema)
